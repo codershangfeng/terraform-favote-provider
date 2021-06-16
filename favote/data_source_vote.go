b4792a14df04a95ed490d179c05dd876fc995fef
Shangfeng Liu <codershangfeng@gmail.com>
2021-06-16 10:15:25 +0800

    Update data source of vote.

--- favote/data_source_vote.go
+++ favote/data_source_vote.go
@@ -17,13 +17,9 @@ func dataSourceVote() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataSourceVoteRead,
 		Schema: map[string]*schema.Schema{
-			"vid": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
 			"topic": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Required: true,
 			},
 			"options": {
 				Type:     schema.TypeList,
@@ -41,7 +37,7 @@ func dataSourceVoteRead(ctx context.Context, d *schema.ResourceData, m interface
 
 	var diags diag.Diagnostics
 
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/vote/%s", "http://localhost:8080", d.Get("vid")), nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/votes", "http://localhost:8080"), nil)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -52,36 +48,36 @@ func dataSourceVoteRead(ctx context.Context, d *schema.ResourceData, m interface
 	}
 	defer r.Body.Close()
 
-	if r.StatusCode == http.StatusNotFound {
-		d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
-		return diags
-	}
-
-	v := new(VoteDataSource)
-	err = json.NewDecoder(r.Body).Decode(&v)
+	votes := make([]VoteDataSource, 0)
+	err = json.NewDecoder(r.Body).Decode(&votes)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	if err := d.Set("vid", strconv.Itoa(v.Id)); err != nil {
-		return diag.FromErr(err)
-	}
+	topic := d.Get("topic").(string)
 
-	if err := d.Set("topic", v.Topic); err != nil {
-		return diag.FromErr(err)
-	}
+	d.SetId("")
 
-	if err := d.Set("options", v.Options); err != nil {
-		return diag.FromErr(err)
-	}
+	for _, v := range votes {
+		if v.Topic == topic {
+			if err := d.Set("topic", v.Topic); err != nil {
+				return diag.FromErr(err)
+			}
 
-	d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
+			if err := d.Set("options", v.Options); err != nil {
+				return diag.FromErr(err)
+			}
+
+			d.SetId(strconv.Itoa(v.ID))
+			break
+		}
+	}
 
 	return diags
 }
 
 type VoteDataSource struct {
-	Id      int      `json:"id"`
+	ID      int      `json:"id"`
 	Topic   string   `json:"topic"`
 	Options []string `json:"options"`
 }
