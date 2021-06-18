e5de2b2aa3d0c7a12598b8522d65cd7317cb448a
Shangfeng Liu <codershangfeng@gmail.com>
2021-06-18 08:49:18 +0800

    Init demo.

--- examples/main.tf
+++ examples/main.tf
@@ -7,16 +7,4 @@ terraform {
   }
 }
 
-data "favote_vote" "vote_1" {
-  topic = "What's your favorite sports?"
-}
 
-# Returns all favorites votes
-output "vote_1" {
-  value = data.favote_vote.vote_1
-}
-
-resource "favote_vote" "name" {
-  topic   = "What's your favorite sports?"
-  options = ["Football", "Basketball", "Tennis"]
-}
